e87dc15ed1a9f549e3b1e2fbdd40a6a9f4a68975
frand-lebaza <frandlebaza@gmail.com>
2025-09-15 20:59:09 -0500

    simulación cajero

--- 01_basics/excercise_one.py
+++ 01_basics/excercise_one.py
@@ -20,6 +20,27 @@ def nodo_consultar(state: ConversationState) ->  ConversationState:
         mensaje=f"Tu saldo actual es: ${state.saldo}"
     )
 
+def nodo_retirar(state: ConversationState) -> ConversationState:
+    return ConversationState(
+        user_input=state.user_input,
+        mensaje="¿Cuánto dinero deseas retirar?: ",
+        saldo = state.saldo - int(input("Ingresa el saldo a retirar: "))  
+    )
+    
+def nodo_depositar(state: ConversationState) -> ConversationState:
+    return ConversationState(
+        user_input=state.user_input,
+        mensaje=f"¿Cuánto dinero deseas depositar? ",
+        saldo= state.saldo + int(input("Ingresa el saldo a depositar: "))        
+    )
+
+def nodo_saldo(state: ConversationState) -> ConversationState:
+    return ConversationState(
+        user_input=state.user_input,
+        mensaje=f"Tu nuevo saldo es: ${state.saldo}",
+        saldo=state.saldo
+    )
+    
 def nodo_default(state: ConversationState) -> ConversationState:
     return ConversationState(
         user_input=state.user_input,
@@ -32,6 +53,10 @@ def decidir_ruta(state: ConversationState) -> str:
     texto = state.user_input.lower().strip()
     if texto == "consultar":
         return "consultar"
+    elif texto == "retirar":
+        return "retirar"
+    elif texto == "depositar":
+        return "depositar"
     else:
         return "default"
     
@@ -39,6 +64,9 @@ graph = StateGraph(ConversationState)
 
 graph.add_node("inicio", nodo_inicio)
 graph.add_node("consultar", nodo_consultar)
+graph.add_node("retirar", nodo_retirar)
+graph.add_node("depositar", nodo_depositar)
+graph.add_node("saldo", nodo_saldo)
 graph.add_node("default", nodo_default)
 
 graph.set_entry_point("inicio")
@@ -48,11 +76,15 @@ graph.add_conditional_edges(
     decidir_ruta,
     {
         "consultar": "consultar",
+        "retirar": "retirar",
+        "depositar": "depositar",        
         "default": "default",
     }
 )
 
-graph.add_edge("consultar", END)
+graph.add_edge("consultar", "saldo")
+graph.add_edge("retirar", "saldo")
+graph.add_edge("depositar", "saldo")
 graph.add_edge("default", END)
 
 app = graph.compile()
